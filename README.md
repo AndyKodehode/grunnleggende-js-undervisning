0c2bc1d4649d055b00609e029fd026f8338760b6
AndyKodehode <git@stackblitz.com>
2024-11-08 09:27:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# grunnleggende-js-undervisning
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AndyKodehode/grunnleggende-js-undervisning)
---
+++ script.js
@@ -0,0 +1,27 @@
+// Hvordan hente inn ting vi skal gjøre noe med
+
+/* .querySelection() */
+// Legge det inn i variabler, bokser til å legge ting i
+
+//Datatyper
+
+//Console.log
+
+//Hvordan skrive en funksjon
+
+// Argumenter vs parametre
+
+/*
+function add(a, b) { return a + b; } */
+
+//Scope
+
+//Legge innhold til en text
+
+//Simple calculator
+
+// classList.add
+
+//classList.toggle
+
+//eventListener
